84d22e2b101efe5945974b26cd1f921244562722
jsiggia-38 <jsiggia38@gmail.com>
2022-03-04 22:05:06 -0500

    added flixster part 2 files

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,19 +1,28 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+
+@Parcelize
 data class Movie(
 
-    val movieID: Int,
+    val movieId: Int,
     val title: String,
     val overview: String,
+    val voteAverage: Double,
     private val posterPath: String,
 
 
 
 
-) {
+) : Parcelable {
+
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
             val movies = mutableListOf<Movie>()
@@ -24,6 +33,7 @@ data class Movie(
                         movieJson.getInt("id"),
                         movieJson.getString("title"),
                         movieJson.getString("overview"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path")
                     )
                 )
@@ -34,3 +44,4 @@ data class Movie(
     }
 
 }
+
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -26,16 +29,28 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     override fun getItemCount() = movies.size
 
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
 
     }
